37899793c5684b32a247b48e742e6fff0a87d6f0
swiftleah <leahrosehenry@gmail.com>
2023-10-10 02:06:51 -0700

    Task 3 - added save(self): and to_dict(self):

--- models/base_model.py
+++ models/base_model.py
@@ -17,6 +17,26 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
-    def time_update(self):
+    def save(self):
         """ Defining time when instance is updated """
         self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """ contains all keys/values of __dict__ of instance.
+        added key class with class name of specific object.
+        created_at and updated_at are converted with ISO format """
+        class_name = self.__class__.__name__
+        instance_dict = self.__dict__
+        format_created_at = self.created_at.isoformat()
+        format_updated_at = self.updated_at.isoformat()
+
+        instance_dict['__class__'] = class_name
+        instance_dict['created_at'] = format_created_at
+        instance_dict['updated_at'] = format_updated_at
+        return instance_dict
+
+
+    def __str__(self):
+        """ string representation of BaseModel """
+        class_name = self.__class__.__name__
+        return f'({class_name}, {self.id}, {self.__dict__})
