5fcf92183460bbbee91a8bf146a8d060ab8326fc
swiftleah <leahrosehenry@gmail.com>
2023-10-10 00:35:38 -0700

    Part of task 3 - created base model with attributes (id, created_at, updated_at)

---
+++ models/base_model.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Parent class BaseModel with common attributes for other classes """
+
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """ class BaseModel
+    assigns UUID to an instance
+    keeps track of time when instance is created
+    and updated """
+    def __init__(self):
+        """ Defining attributes above """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def time_updated(self):
+        """ Defining time when instance is updated """
+        self.updated_at = datetime.now()
