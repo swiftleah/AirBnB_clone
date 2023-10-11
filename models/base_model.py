6b4dd4bfb2bfa5c303a1dc8f81f559ed82e1e5f3
swiftleah <leahrosehenry@gmail.com>
2023-10-11 01:41:57 -0700

    Task 4: altered method '__init__' to include args and kwargs. if kwargs not empty: we are creating an instance from dictionary where special key '__class__' is ignored, 'created_at' and 'updated_at' are converted into datetime objects using 'datetime.strptime()'. if kwargs is empty, create instance as previously did.

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,27 @@ class BaseModel:
     assigns UUID to an instance
     keeps track of time when instance is created
     and updated """
-    def __init__(self):
-        """ Defining attributes above """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        """ Defining attributes above.
+        if kwargs not empty: creating instance from
+        dictionary. 'created_at' and 'updated_at' converted into
+        datetime objects using datetime.strptime().
+
+        if kwargs empty - creating normal instance where 'id',
+        'created_at' and 'updated_at' is set. """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                setattr(self, key, value)
+            if 'created_at' in kwargs:
+                self.created_at = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            if 'updated_at' in kwargs:
+                self.updated_at = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def save(self):
         """ Defining time when instance is updated """
