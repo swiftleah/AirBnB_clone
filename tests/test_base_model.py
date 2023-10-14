deda21a2fbac3e2f235d6d0513fff1712f8a5f39
swiftleah <leahrosehenry@gmail.com>
2023-10-14 05:23:50 -0700

    More test for task 3

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -7,6 +7,7 @@ import unittest
 from models.base_model import BaseModel
 from datetime import datetime
 import models
+from time import sleep
 
 
 class TestBaseModel_instances(unittest.TestCase):
@@ -14,3 +15,15 @@ class TestBaseModel_instances(unittest.TestCase):
 
     def test_no_arg_instance(self):
         self.assertEqual(BaseModel, type(BaseModel()))
+
+    def test_two_models_created_different_times(self):
+        Model1 = BaseModel()
+        sleep(0.1)
+        Model2 = BaseModel()
+        self.assertNotEqual(Model1.created_at, Model2.created_at)
+
+    def test_two_models_updated_different_times(self):
+        Model1 = BaseModel()
+        sleep(0.1)
+        Model2 = BaseModel()
+        self.assertNotEqual(Model1.updated_at, Model2.updated_at)
