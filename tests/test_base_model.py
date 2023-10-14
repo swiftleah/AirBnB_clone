933a80b420d472167737848b16899d70f00e0e68
swiftleah <leahrosehenry@gmail.com>
2023-10-14 05:56:49 -0700

    More tests

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -11,7 +11,7 @@ from time import sleep
 
 
 class TestBaseModel_instances(unittest.TestCase):
-    """ tests the creation of instances from parent class Base Model """
+    """ tests the creation of instances from parent class BaseModel """
 
     def test_no_arg_instance(self):
         self.assertEqual(BaseModel, type(BaseModel()))
@@ -29,7 +29,7 @@ class TestBaseModel_instances(unittest.TestCase):
         self.assertNotEqual(Model1.updated_at, Model2.updated_at)
 
 class TestBaseModel_save(unittest.TestCase):
-    """ tests for 'save' in parent class Base Model """
+    """ tests for 'save' in parent class BaseModel """
 
     def test_save(self):
         Model = BaseModel()
@@ -37,3 +37,19 @@ class TestBaseModel_save(unittest.TestCase):
         first_updated_at = Model.updated_at
         Model.save()
         self.assertLess(first_updated_at, Model.updated_at)
+
+class TestBaseModel_to_dict(unittest.TestCase):
+    """ tests for 'to_dict' in parent class BaseModel """
+
+    def test_type_dict(self):
+        M = BaseModel()
+        self.assertEqual(dict, type(M.to_dict()))
+
+    def test_dict_datetimes_are_strings(self):
+        M = BaseModel()
+
+        self.assertEqual(str, type(M.to_dict()["created_at"]))
+        self.assertEqual(str, type(M.to_dict()["updated_at"]))
+
+if __name__ == "__main__":
+    unittest.main()
