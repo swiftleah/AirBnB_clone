e8e2b90932fedc5e13eb42d6f1acaa144dfcccbc
swiftleah <leahrosehenry@gmail.com>
2023-10-14 10:34:19 -0700

    Returned to previous

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,8 @@ class BaseModel:
             *args & **kwargs
             if **kwargs: creates instance from dict
         """
-
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
         self.id = str(uuid.uuid4())
         time = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
@@ -24,8 +25,6 @@ class BaseModel:
                 if key == "created_at" or key == "updated_at":
                     self.__dict__[key] = datetime.strptime(value, time)
         else:
-            self.created_at = datetime.now()
-           self.updated_at = self.created_at
             models.storage.new(self)
 
     def save(self):
