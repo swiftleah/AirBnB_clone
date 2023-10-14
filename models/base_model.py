8a582d4a09329aea1dfb9167c6e5664cb7afd209
swiftleah <leahrosehenry@gmail.com>
2023-10-14 06:06:09 -0700

    Added check for key '__class__' in basemodel.py

--- models/base_model.py
+++ models/base_model.py
@@ -20,6 +20,8 @@ class BaseModel:
         time = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
                 if key == "created_at" or key == "updated_at":
                     self.__dict__[key] = datetime.strptime(value, time)
         else:
