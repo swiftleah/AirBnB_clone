e8b36bdf6760ee41ae7da1cfe751759f1ffeeed5
swiftleah <leahrosehenry@gmail.com>
2023-10-14 08:28:27 -0700

    Moved initialization of attribute 'id' before checking kwargs in basemodel.py

--- models/base_model.py
+++ models/base_model.py
@@ -17,6 +17,7 @@ class BaseModel:
             if **kwargs: creates instance from dict
         """
 
+        self.id = str(uuid.uuid4())
         time = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key, value in kwargs.items():
@@ -25,7 +26,6 @@ class BaseModel:
                 if key == "created_at" or key == "updated_at":
                     self.__dict__[key] = datetime.strptime(value, time)
         else:
-            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
             models.storage.new(self)
