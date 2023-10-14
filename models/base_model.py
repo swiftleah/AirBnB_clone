4cce74dabdc033e32d36089e7beab460bcf71ac4
swiftleah <leahrosehenry@gmail.com>
2023-10-14 04:52:06 -0700

    Simplified coding for basemodel.py - output is now correct in terms of format of time

--- models/base_model.py
+++ models/base_model.py
@@ -20,17 +20,11 @@ class BaseModel:
 
         if kwargs empty - creating normal instance where 'id',
         'created_at' and 'updated_at' is set. """
+        time = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                setattr(self, key, value)
-            if 'created_at' in kwargs:
-                self.created_at = datetime.strptime(kwargs['created_at'],
-                                                    "%Y-%m-%dT%H:%M:%S.%f")
-            if 'updated_at' in kwargs:
-                self.updated_at = datetime.strptime(kwargs['updated_at'],
-                                                    "%Y-%m-%dT%H:%M:%S.%f")
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, time)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -46,24 +40,14 @@ class BaseModel:
         """ contains all keys/values of __dict__ of instance.
         added key class with class name of specific object.
         created_at and updated_at are converted with ISO format """
-        if not isinstance(self.created_at, datetime):
-            self.created_at = datetime.strptime(self.created_at,
-                                                "%Y-%m-%dT%H:%M:%S.%f")
-        if not isinstance(self.updated_at, datetime):
-            self.updated_at = datetime.strptime(self.updated_at,
-                                                "%Y-%m-%dT%H:%M:%S.%f")
 
-        class_name = self.__class__.__name__
-        instance_dict = self.__dict__
-        format_created_at = self.created_at.isoformat()
-        format_updated_at = self.updated_at.isoformat()
+        instance_dict = self.__dict__.copy()
 
-        instance_dict['__class__'] = class_name
-        instance_dict['created_at'] = format_created_at
-        instance_dict['updated_at'] = format_updated_at
+        instance_dict['__class__'] = self.__class__.__name__
+        instance_dict['created_at'] = self.created_at.isoformat()
+        instance_dict['updated_at'] = self.updated_at.isoformat()
         return instance_dict
 
     def __str__(self):
         """ string representation of BaseModel """
-        class_name = self.__class__.__name__
-        return f'({class_name}, {self.id}, {self.__dict__})'
+        return f'({self.__class__.__name__}, {self.id}, {self.__dict__})'
