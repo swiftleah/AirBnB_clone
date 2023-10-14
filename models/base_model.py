97529c289e73b513967fb693c53ed684201174c6
swiftleah <leahrosehenry@gmail.com>
2023-10-14 10:33:04 -0700

    Testing

--- models/base_model.py
+++ models/base_model.py
@@ -18,14 +18,14 @@ class BaseModel:
         """
 
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
         time = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     self.__dict__[key] = datetime.strptime(value, time)
         else:
+            self.created_at = datetime.now()
+           self.updated_at = self.created_at
             models.storage.new(self)
 
     def save(self):
