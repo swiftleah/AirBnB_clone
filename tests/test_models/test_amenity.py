13ce3b50765558750f2b988c69fcf97070df6f78
swiftleah <leahrosehenry@gmail.com>
2023-10-14 10:42:36 -0700

    Starting tests for amenity.py

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" unittests for amenity.py """
+
+
+import os
+import unittest
+import models
+from datetime import datetime
+from time import sleep
+from models.amenity import Amenity
+
+
+class TestAmenity_instance(unittest.TestCase):
+    """ unittesting for instances of amenity """
+
+    def test_amenity_type(self):
+        self.assertIn(Amenity(), models.storage.all().values())
