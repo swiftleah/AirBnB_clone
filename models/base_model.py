cde022a0f57b0832dc3a4f178eab0a8ee2e3de5a
swiftleah <leahrosehenry@gmail.com>
2023-10-14 08:31:28 -0700

    Did the same thing with created_at and updated_at

--- models/base_model.py
+++ models/base_model.py
@@ -18,6 +18,8 @@ class BaseModel:
         """
 
         self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
         time = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key, value in kwargs.items():
@@ -26,8 +28,6 @@ class BaseModel:
                 if key == "created_at" or key == "updated_at":
                     self.__dict__[key] = datetime.strptime(value, time)
         else:
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
             models.storage.new(self)
 
     def save(self):
