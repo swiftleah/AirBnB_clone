5f3924bc3475bbed8e6513727c60b0323f07909d
swiftleah <leahrosehenry@gmail.com>
2023-10-15 01:30:23 -0700

    imported classes state, city, etc to FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,6 +6,11 @@ and deserialize to instances """
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.review import Review
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
 
 
 class FileStorage:
