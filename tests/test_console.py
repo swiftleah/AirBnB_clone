122475c17b3f6cab40558962c58a190b0e79aebf
swiftleah <leahrosehenry@gmail.com>
2023-10-16 02:55:11 -0700

    Added more unittests

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,6 +4,8 @@
 
 import os
 import unittest
+from unittest.mock import patch
+from io import StringIO
 from models import storage
 from models.engine.file_storage import FileStorage
 import sys
@@ -15,3 +17,37 @@ class TestHBNBCommand_prompting(unittest.TestCase):
 
     def test_prompt_str(self):
         self.assertTrue("(hbnb) ", HBNBCommand.prompt)
+
+class TestHBNBCommand_help(unittest.TestCase):
+    """ unittesting for command help on shell """
+
+    def test_help_EOF(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("help EOF"))
+            self.assertEqual("exits program when input is 'EOF'", output.getvalue().strip())
+
+class TestHBNBCommand_exit(unittest.TestCase):
+    """ unittesting for exit command in shell """
+
+    def test_quit_command(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertTrue(HBNBCommand().onecmd("quit"))
+
+    def test_EOF_command(self):
+        """ seeing if input 'EOF' causes shell to quit """
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertTrue(HBNBCommand().onecmd("EOF"))
+
+class TestHBNBCommand_create(unittest.TestCase):
+    """ unittesting 'create' command which creates instance based
+        off parent class BaseModel """
+
+    def test_create_no_class_name_given(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("create"))
+            self.assertEqual("** class name missing **", output.getvalue().strip())
+
+
+
+if __name__ == "__main__":
+    unittest.main()
