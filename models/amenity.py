4d0c73080d31af386aa66ec1ce55341826fd6074
root <RobinLeeHenry101@gmail.com>
2023-10-16 02:57:55 -0700

    trying to fix the amenity file

--- models/amenity.py
+++ models/amenity.py
@@ -8,3 +8,7 @@ from models.base_model import BaseModel
 class Amenity(BaseModel):
     """ Amenity class that inherits from BaseModel """
     name = ""
+
+    def __str__(self):
+        """ Return a string of two characters """
+        return self.name[:2].ljust(2)
